1fc36f703b1214fe76ada1fe931748f5ca4167b5
jordanrios94 <jordanrios.software@gmail.com>
2018-08-12 11:27:17 +0100

    feat: introduced auth state to app for managing the user

---
+++ client/src/components/App.js
@@ -0,0 +1,34 @@
+// Rendering layer control (React Router)
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import Header from './Header';
+import Landing from './Landing';
+const Dashboard = () => <div className="container"><h2>Dashboard</h2></div>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route path="/" exact component={Landing} />
+                        <Route path="/surveys" exact component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default connect(null, actions)(App);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Landing = () => (
+    <div className="container center-align">
+        <h2>Emaily!</h2>
+        Collect feedback from your users
+    </div>
+);
+
+export default Landing;
