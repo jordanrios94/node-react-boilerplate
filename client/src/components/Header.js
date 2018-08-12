6803ac4d4ec26375b32e2efca97df1892e36b9ef
jordanrios94 <jordanrios.software@gmail.com>
2018-08-12 18:01:46 +0100

    feat: credits is now shown in the navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -14,6 +14,7 @@ class Header extends Component {
                 return (
                     <Fragment>
                         <li><Payments /></li>
+                        <li style={{ margin: '0 10px' }}>{`Credits: ${this.props.auth.credits}`}</li>
                         <li><a href="/api/logout">Logout</a></li>
                     </Fragment>
                 );
